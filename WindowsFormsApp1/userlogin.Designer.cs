5e444d0a7bf75fada467beab4d122c2cde0ac50e
Burhan Arslanbaş <burhanim038@outlook.com>
2022-06-25 19:15:55 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/userlogin.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class userlogin
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.SuspendLayout();
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(313, 139);
+            this.txtuser.Multiline = true;
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(100, 20);
+            this.txtuser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Location = new System.Drawing.Point(229, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(313, 176);
+            this.txtpass.Multiline = true;
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(100, 20);
+            this.txtpass.TabIndex = 2;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnlogin.Location = new System.Drawing.Point(232, 217);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(181, 23);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=arslanbas.database.windows.net;Initial Catalog=deneme;Persist Securit" +
+    "y Info=True;User ID=login;Password=Burhan123";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // userlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtuser);
+            this.Name = "userlogin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.userlogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+    }
+}
+
---
+++ WindowsFormsApp1/userlogin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class userlogin : Form
+    {
+        public userlogin()
+        {
+            InitializeComponent();
+        }
+        public string username, password;
+        private void userlogin_Load(object sender, EventArgs e)
+        {
+            // MessageBox.Show(sqlConnection1.State.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kontrol();
+        }
+        public void kontrol()
+        {
+            sqlConnection1.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users");
+            cmd.Connection = sqlConnection1;
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                if (dr["username"].ToString() == txtuser.Text)
+                {
+                    if (dr["password"].ToString() == txtpass.Text)
+                    {
+                        MessageBox.Show("Bilgileriniz sistemde kayıtlıdır", "Giriş Başarılı");
+                    }
+                }
+                // username = (string)dr["username"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı Veya Şifre'yi Kontrol Ediniz!");
+            }
+            sqlConnection1.Close();
+        }
+    }
+}
+  
